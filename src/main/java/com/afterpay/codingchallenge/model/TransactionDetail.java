27e5db428b6dce0d083f68d219edcd43743f29f7
Mohit <mohit.aggarwal.au@gmail.com>
2019-12-07 14:53:03 +1100

    WIP : added code for coding challenge

---
+++ src/main/java/com/afterpay/codingchallenge/model/TransactionDetail.java
@@ -0,0 +1,36 @@
+package com.afterpay.codingchallenge.model;
+
+import java.time.Instant;
+
+public class TransactionDetail {
+
+    private String hashedCardNumber;
+
+    private Instant transactionTime;
+
+    private double transactionAmount;
+
+    public String getHashedCardNumber() {
+        return hashedCardNumber;
+    }
+
+    public void setHashedCardNumber(String hashedCardNumber) {
+        this.hashedCardNumber = hashedCardNumber;
+    }
+
+    public Instant getTransactionTime() {
+        return transactionTime;
+    }
+
+    public void setTransactionTime(Instant transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(double transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+}
---
+++ src/test/java/unit/com/afterpay/codingchallenge/application/ReadFileTest.java
@@ -0,0 +1,14 @@
+package unit.com.afterpay.codingchallenge.application;
+
+import com.afterpay.codingchallenge.application.ReadFile;
+import org.junit.Test;
+
+public class ReadFileTest {
+
+    private ReadFile readFile = new ReadFile();
+
+    @Test
+    public void testAlpha() {
+        readFile.betaTesting("sample.txt", 15.00);
+    }
+}
