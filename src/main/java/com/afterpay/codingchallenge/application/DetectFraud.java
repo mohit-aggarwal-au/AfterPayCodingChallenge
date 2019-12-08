4d3ec20f1221f355640acec9326d455f9b33b8c9
Mohit <mohit.aggarwal.au@gmail.com>
2019-12-08 13:03:51 +1100

    WIP : added code for coding challenge

--- src/main/java/com/afterpay/codingchallenge/application/DetectFraud.java
+++ src/main/java/com/afterpay/codingchallenge/application/DetectFraud.java
@@ -16,10 +16,16 @@ public class DetectFraud {
 
         Set<String> fraudSet = new HashSet<>();
         transactionDetails.forEach(detail -> {
-            List<TransactionDetail> slidingTimeWindowList = transactionDetails.stream().filter(isValidTimeWindow(detail.getHashedCardNumber(), detail.getTransactionTime())).collect(Collectors.toList());
+            //Total transaction amount is the sum of all the transaction amount that happened over a 24 hour sliding window period for a given credit card of a transaction
+//            Double totalTransactionAmount = transactionDetails.stream().filter(isValidTimeWindow(detail.getHashedCardNumber(), detail.getTransactionTime())).
+//                    collect(Collectors.summingDouble(TransactionDetail::getTransactionAmount));
 
-            //Double totalTransactionMoney = transactionDetails.stream().filter(isValidTimeWindow(detail.getHashedCardNumber(), detail.getTransactionTime())).collect(Collectors.summingDouble(TransactionDetail::getTransactionAmount));
-            if (isFraudDetected(slidingTimeWindowList, threshold)) {
+            List<TransactionDetail> slidingTimeWindowList = transactionDetails.stream().filter(isValidTimeWindow(detail.getHashedCardNumber(), detail.getTransactionTime())).collect(Collectors.toList());
+            Double totalTransactionAmount = slidingTimeWindowList.stream().collect(Collectors.summingDouble(TransactionDetail::getTransactionAmount));
+
+            if (totalTransactionAmount > threshold) {
+                // There may be duplicate hashed card numbers which were detected as fraudulent over multiple 24 hours sliding window periods, program will return only distinct hashed card values
+                // List will also contain card numbers for which a single transaction amount is more than threshold.
                 fraudSet.add(detail.getHashedCardNumber());
             }
         });
@@ -28,14 +34,8 @@ public class DetectFraud {
 
     private static Predicate<TransactionDetail> isValidTimeWindow(String hashedCreditCardNumber, Instant transactionTime) {
 
-        //For a given hashed card number, TransactionToBeChecked has to be less than or equal to the transaction time and more than or equal to transaction time - 24 hours
+        //For a given hashed card number, TransactionToBeChecked time has to be less than or equal to the transaction time and more than or equal to (transaction time - 24 hours)
         return transactionToBeChecked -> transactionToBeChecked.getHashedCardNumber().equals(hashedCreditCardNumber) &&
                 (transactionTime.compareTo(transactionToBeChecked.getTransactionTime()) >= 0 && transactionTime.minus(Duration.ofHours(24)).compareTo(transactionToBeChecked.getTransactionTime()) <= 0);
     }
-
-    private boolean isFraudDetected(List<TransactionDetail> transactionDetails, double threshold) {
-        Double totalTransactionMoney = transactionDetails.stream().collect(Collectors.summingDouble(TransactionDetail::getTransactionAmount));
-        return totalTransactionMoney >= threshold;
-    }
-
 }
