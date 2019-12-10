ef14e037f9e132bc7a77b0da5028189daeb4b91b
mohit-aggarwal-au <58576883+mohit-aggarwal-au@users.noreply.github.com>
2019-12-10 15:16:25 +1100

    Update CheckFraudulentTransactionsTest.java

--- src/test/java/unit/com/afterpay/codingchallenge/application/CheckFraudulentTransactionsTest.java
+++ src/test/java/unit/com/afterpay/codingchallenge/application/CheckFraudulentTransactionsTest.java
@@ -208,3 +208,39 @@ public class ReadFileUtilTest {
     assertTrue(exception.getMessage().contains("Exception occurred while opening input file"));
   }
 }
+
+  public static void main(String[] args) {
+    InputDetails inputDetails = getInputDetails(args);
+    if (inputDetails != null) {
+      CalculatorService calculatorService = new CalculatorService();
+      BigDecimal convertedAmount = calculatorService.convertMoney(inputDetails);
+      System.out.print(inputDetails.getFromCurrency() + " " + inputDetails.getAmountToBeConverted() + " = " + inputDetails.getToCurrency() + " " + convertedAmount);
+    }
+
+  }
+
+  private static InputDetails getInputDetails(String[] args) {
+    if (args == null || args.length != 4) {
+      throw new InvalidParameterException("Input parameters are not correct");
+    }
+
+    InputDetails inputDetails = null;
+    try {
+      Currency fromCurrency = Currency.valueOf(args[0].trim());
+      Currency toCurrency = Currency.valueOf(args[3].trim());
+      BigDecimal amountToBeConverted = new BigDecimal(args[1].trim()).setScale(2, RoundingMode.HALF_UP);
+      inputDetails = new InputDetails(fromCurrency, toCurrency, amountToBeConverted);
+
+    } catch (NumberFormatException exception) {
+      System.out.print(String.format("Exception occurred while parsing input values:" + exception.getMessage()));
+    } catch (IllegalArgumentException exception) {
+      System.out.print(String.format("Unable to find rate for %s/%s", args[0].trim().toUpperCase(), args[3].trim().toUpperCase()));
+      return null;
+    } catch (Exception exception) {
+      throw new InvalidParameterException("Exception occurred while parsing input values:" + exception.getMessage());
+    }
+
+    return inputDetails;
+  }
+}
+
